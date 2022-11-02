e870495a3cc810a84503f3b19dc55b74b31b61f8
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2022-11-02 14:16:46 +0300

    İyi Bir Programcı Serisi : Constructor Yapısını Anlamak

---
+++ Constructors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Sayit Sami",
+                LastName = "Koca",
+                City = "Aksaray"
+            };
+
+            Customer customer2 = new Customer(2, "Kevin", "Mittnick", "New York");
+
+            Console.WriteLine(customer2.FirstName + " " +customer2.LastName);
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+
+        // Default Constructor
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
