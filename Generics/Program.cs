706e615ef189f94c41abb1997f702d0273d9fd6d
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2022-11-04 19:42:13 +0300

    4. Gün Ödevi | Generic Yapıları Yapısal Mantığıyla Anlamak

---
+++ Generics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // MyList
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Sakarya");
+            sehirler2.Add("Aksaray");
+            sehirler2.Add("Tekirdağ");
+            sehirler2.Add("İzmir");
+            sehirler2.Add("Çanakkale");
+            sehirler2.Add("Samsun");
+
+            Console.WriteLine(sehirler2.Count);
+
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray; 
+
+        public MyList()
+        {
+            _array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+}
