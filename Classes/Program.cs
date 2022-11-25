1cb76bde426d6ec750fae2ec06e5c1f6fc12c0d4
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2022-11-25 18:22:53 +0300

    Kampı Destekleme Ödevleri - Pratikler

---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Mark";
+            customer.LastName = "Zuckerberg";
+            customer.City = "New York";
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                FirstName = "Bill",
+                LastName = "Gates",
+                City = "Washington"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+}
