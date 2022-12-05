64eea1fc19018064cd5909db855e99b9561ebdcf
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2022-12-06 00:20:47 +0300

    Starbucks Kahve alan müşterilerine yıldız ekleyecek - operasyonu yazıldı.

---
+++ InterfaceAbstractDemo/Abstract/IAddStar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    public interface IAddStar
+    {
+        // Kahve alan müşteriye yıldız eklenecek.
+        void AddStar(Customer customer);
+    }
+}
