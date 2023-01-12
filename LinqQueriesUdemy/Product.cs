20a3388b0474e24e6d96f5d8c57a00baf6c9d82e
Sayit Sami Koca <sayitsamikoca0@gmail.com>
2023-01-12 19:44:40 +0300

    Kamp 7. Gün Ödevi Tamamlandı! - LINQ Queries

---
+++ LinqQueriesUdemy/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace LinqQueriesUdemy
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public long UnitsInStock { get; set; }
+
+    }
+}
